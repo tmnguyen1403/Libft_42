13d20c6918e8aceb075617371145dad04aa8a660
Tom Nguyen <tomriddle1403@gmail.com>
2019-09-17 23:27:11 -0500

    Done strlen

--- srcs/libft.h
+++ srcs/libft.h
@@ -4,8 +4,9 @@
 # include <stdio.h>
 # include <stdlib.h>
 
-void	*ft_memset(void *b, int c, size_t len);
-void	ft_bzero(void *s, size_t n);
-void	*ft_memcpy(void *dst, void *src, size_t n);
-void	*ft_memccpy(void *dst, void *src, int c, size_t n);
+void		*ft_memset(void *b, int c, size_t len);
+void		ft_bzero(void *s, size_t n);
+void		*ft_memcpy(void *dst, void *src, size_t n);
+void		*ft_memccpy(void *dst, void *src, int c, size_t n);
+size_t	ft_strlen(const char *s);
 #endif
