cd6403140122fddfa637bd9869c618eb44084843
Tom Nguyen <tomriddle1403@gmail.com>
2019-09-22 12:01:13 -0500

    Done lstdelone, tested

---
+++ srcs/ft_lstdelone.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+
+/**free the memory of the link's content using the function del
+**free the link's memory using free
+**the memory of next must not be freed
+**the pointer to the link that was just freed is set to null
+**/
+void		ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
+{
+	t_list	*tmp;
+
+	tmp = *alst;
+	del(tmp->content, tmp->content_size);
+	free(tmp);
+	*alst = 0;
+}
--- srcs/libft.h
+++ srcs/libft.h
@@ -65,6 +65,7 @@ typedef struct	s_list
 	struct s_list		*next;
 }								t_list;
 #include "ft_lstnew.c"
+#include "ft_lstdelone.c"
 void		*ft_memset(void *b, int c, size_t len);
 void		ft_bzero(void *s, size_t n);
 void		*ft_memcpy(void *dst, void *src, size_t n);
@@ -118,4 +119,5 @@ void		ft_putstr_fd(char const *s, int fd);
 void		ft_putendl_fd(char const *s, int fd);
 void		ft_putnbr_fd(int n, int fd);
 t_list	*ft_lstnew(void const *content, size_t content_size);
+void		ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
 #endif
