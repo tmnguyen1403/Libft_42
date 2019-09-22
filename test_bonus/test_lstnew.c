4ad39e3cd008f8670ff9f3ae295c7bc463ce8562
Tom Nguyen <tomriddle1403@gmail.com>
2019-09-22 11:44:02 -0500

    Done lstnew, Tested

--- test_bonus/test_lstnew.c
+++ test_bonus/test_lstnew.c
@@ -29,12 +29,17 @@ void	test_nb(const void *content, size_t content_size)
 		++index;
 	}
 	node = ft_lstnew(content , content_size);
-	// if (node)
-	// {
-	// 	printf("has node, content: %s, content_size: %lu\n", (char *)node->content, node->content_size);
-	// 	free(node);
-	// 	node = 0;
-	// }
+	if (node)
+	{
+		printf("result node, size: %lu\n", node->content_size);
+		index = 0;
+		arr = (int *)node->content;
+		while (index < node->content_size / 4)
+		{
+			printf("a[%lu] = %d\n", index, arr[index]);
+			++index;
+		}
+	}
 }
 
 int main(void)
@@ -45,5 +50,12 @@ int main(void)
 	test_str("abcd", 4);
 	test_str("abcd", 5);
 	test_str("abcd", 6);
+
+	int a[1] = {15};
+	test_nb((void *)a, 4);
+	int *b = 0;
+	test_nb((void *)b, 1);
+	int c[3] = {15, 20, -190};
+	test_nb((void *)c, 12);
 	return (0);
 }
